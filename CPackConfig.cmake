b468625f9260dc6ab1f6295f2d60869a5f6ea4b1
EhEhEhEh-labs <new3now@yandex.ru>
2025-05-25 13:03:31 +0300

    Update CPackConfig.cmake

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,29 +1,35 @@
 include(InstallRequiredSystemLibraries)
-set(CPACK_PACKAGE_CONTACT booboo@boo.bo)
-set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
+
+# Общие настройки
+set(CPACK_PACKAGE_CONTACT "booboo@boo.bo")
+set(CPACK_PACKAGE_VERSION "${PRINT_VERSION}")
 set(CPACK_PACKAGE_NAME "solver")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "static C++ library for solver")
 set(CPACK_PACKAGE_VENDOR "ghost")
-set(CPACK_PACKAGE_PACK_NAME "solver-${PRINT_VERSION}")
-set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
+set(CPACK_PACKAGE_FILE_NAME "solver-${PRINT_VERSION}")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+
+# Настройки исходных пакетов
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
 
+# Настройки Debian
 set(CPACK_DEBIAN_PACKAGE_PREDEPENDS "cmake >= 3.0")
 set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
-set(CPACK_DEBIAN_PACKAGE_VERSION ${PRINT_VERSION})
 set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "solves equations")
 
+# Настройки RPM
 set(CPACK_RPM_PACKAGE_SUMMARY "solves equations")
 
-# WIX (MSI) package settings
-set(CPACK_WIX_UPGRADE_GUID "12345678-1234-1234-1234-123456789012")
-set(CPACK_WIX_PRODUCT_GUID "87654321-4321-4321-4321-210987654321")
-
-# DragNDrop (DMG) package settings
-set(CPACK_DMG_VOLUME_NAME "Solver")
-set(CPACK_DMG_FORMAT "UDZO")
-
-set(CPACK_GENERATOR "DEB;RPM;WIX;DragNDrop")
+# Выбор генераторов в зависимости от ОС
+if(WIN32)
+    set(CPACK_GENERATOR "WIX")
+    # Дополнительные настройки для MSI
+    set(CPACK_WIX_PRODUCT_GUID "PUT-YOUR-GUID-HERE") # Обязательно замените на реальный GUID
+elseif(APPLE)
+    set(CPACK_GENERATOR "DragNDrop")
+else()
+    set(CPACK_GENERATOR "DEB;RPM")
+endif()
 
 include(CPack)
